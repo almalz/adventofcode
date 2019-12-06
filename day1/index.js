69a9437606d5b3939093010030f6f616b1323892
alexismalzieu <alex.malzieu@gmail.com>
2019-12-06 23:13:35 +0100

    feat: day #1

---
+++ day1/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const moduleMasses = []
+
+const getFuelRequiredForMass = mass => {
+  const fuel = Math.floor(mass / 3) - 2
+  if (fuel === 0) return fuel
+  if (fuel <= 0) return 0
+
+  return getFuelRequiredForMass(fuel) + fuel
+}
+
+const getTotalFuelRequired = array => {
+  return array.reduce((acc, curr) => acc + getFuelRequiredForMass(curr), 0)
+}
+
+fs.readFile(__dirname + '/inputs', 'utf8', function(err, data) {
+  if (err) throw err
+  moduleMasses.push(...data.split('\n'))
+  console.log(getTotalFuelRequired(moduleMasses))
+})
