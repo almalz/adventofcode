93b8a52d44df33fa1d218435a1d89b7c7ce88c05
alexismalzieu <alex.malzieu@gmail.com>
2019-12-09 23:14:29 +0100

    feat: listed all traces of a wire

--- day3/index.js
+++ day3/index.js
@@ -1,26 +1,94 @@
 const fs = require('fs')
 
-const getPathTraces = wire => {}
+const range = (start, stop, step) =>
+  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
+
+const getSectionListOfCoordinates = ({ orientation, length }, initialPosition) => {
+  let positions
+  let startX = Math.abs(initialPosition[0])
+  let startY = Math.abs(initialPosition[1])
+
+
+  switch (orientation) {
+    case 'U':
+      positions = range(startX, startX + length, 1).map(
+        coord => {
+          return [
+            initialPosition[0] + coord,
+            initialPosition[1]
+          ]
+        }
+      )
+      break
+
+    case 'D':
+      positions = range(startX, startX + length, 1).map(
+        coord => {
+          return [
+            initialPosition[0] - coord,
+            initialPosition[1]
+          ]
+        }
+      )
+      break
+
+    case 'L':
+      positions = range(startY, startY + length, 1).map(
+        coord => {
+          return [
+            initialPosition[0],
+            initialPosition[1] - coord
+          ]
+        }
+      )
+      break
+
+    case 'R':
+      positions = range(startY, startY + length, 1).map(
+        coord => {
+          return [
+            initialPosition[0],
+            initialPosition[1] + coord
+          ]
+        }
+      )
+      break
+
+    default:
+      break
+
+  }
+
+
+  return positions
+}
+
+const getPathTraces = wire => {
+  let traces = [], positions
+  let initialPosition = [0, 0]
+
+  for (const section of wire) {
+    positions = getSectionListOfCoordinates(
+      section,
+      initialPosition
+    )
+    initialPosition = positions[positions.length - 1]
+    traces.push(...positions)
+  }
+  return traces
+}
 
 fs.readFile(__dirname + '/inputs', 'utf8', (err, data) => {
   if (err) throw err
 
-  const wires = data.split('\n')
-  let [wire1, wire2] = [...[wires[0].split(',')], [...wires[1].split(',')]]
-
-  wire1 = wire1.map(section => {
-    return {
-      orientation: section.charAt(0),
-      length: section.substring(1)
-    }
+  const [wire1, wire2] = data.split('\n').map(wire => {
+    return wire.split(',').map(section => {
+      return {
+        orientation: section.charAt(0),
+        length: Number(section.substring(1))
+      }
+    })
   })
 
-  wire2 = wire2.map(section => {
-    return {
-      orientation: section.charAt(0),
-      length: section.substring(1)
-    }
-  })
-
-  console.log({ wire1 })
+  const [trace1, trace2] = [getPathTraces(wire1), getPathTraces(wire2)]
 })
